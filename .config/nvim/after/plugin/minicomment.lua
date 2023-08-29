b29e831e83425701c108b450ea4b004d445857d6
Andreas Palm <andreaspalm98@gmail.com>
2023-08-29 10:39:38 +0200

    Integrate nvim-ts-context-commentstring with mini.comment

--- .config/nvim/after/plugin/minicomment.lua
+++ .config/nvim/after/plugin/minicomment.lua
@@ -1,1 +1,7 @@
-require('mini.comment').setup()
+require('mini.comment').setup({
+    options = {
+        custom_commentstring = function()
+            return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
+        end,
+    },
+})
