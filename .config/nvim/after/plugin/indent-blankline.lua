2cc3bfc1412590c10bbb1c0b299fa6daa19b769f
Andreas Palm <andreaspalm98@gmail.com>
2023-08-30 13:24:29 +0200

    Active indent-blankline for json and csharp

--- .config/nvim/after/plugin/indent-blankline.lua
+++ .config/nvim/after/plugin/indent-blankline.lua
@@ -1,5 +1,5 @@
 
-vim.g.indent_blankline_filetype = {'yaml', 'yml', 'html'}
+vim.g.indent_blankline_filetype = {'yaml', 'yml', 'html', 'json', 'cs'}
 
 vim.cmd [[hi IndentBlanklineChar guifg=#504945 gui=nocombine]]
 
