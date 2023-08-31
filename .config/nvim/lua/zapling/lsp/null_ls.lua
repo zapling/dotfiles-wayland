bb375900f8aeac541bd013ec58ba9d850505c471
Andreas Palm <andreaspalm98@gmail.com>
2023-08-31 11:58:45 +0200

    Remove old zimpler null_ls config

--- .config/nvim/lua/zapling/lsp/null_ls.lua
+++ .config/nvim/lua/zapling/lsp/null_ls.lua
@@ -3,26 +3,8 @@ local Job = require'plenary.job'
 
 local M = {}
 
--- local zimpler_null_ls_config = {
---     args = {
---         "go",
---         "lint",
---         "netscape",
---         "--",
---         "--fix=false",
---         "--build-tags=integration_test",
---         "--out-format=json",
---         "$DIRNAME",
---         "--path-prefix",
---         "$ROOT"
---     },
--- }
-
 M.config = {
     sources = {
-        -- require("zimpler.null_ls").diagnostics_go_lint("netscape").with(zimpler_null_ls_config),
-        -- require("zimpler.null_ls").diagnostics_go_lint("napster").with(zimpler_null_ls_config),
-
         null_ls.builtins.diagnostics.golangci_lint.with({
             args = {
                 "run",
@@ -33,15 +15,11 @@ M.config = {
                 "--path-prefix",
                 "$ROOT"
             },
-            runtime_condition = function()
-                return not vim.g.arthur_diagnostics_go_lint_loaded
-            end
         }),
 
         -- shell / bash
         null_ls.builtins.diagnostics.shellcheck,
 
-        -- typescript
         null_ls.builtins.formatting.eslint_d.with({
             on_attach = function()
                 Job:new({command = 'eslint_d', args = {'restart'}}):sync()
@@ -64,8 +42,6 @@ M.config = {
                 return utils.root_has_file({".eslintrc"})
             end,
         }),
-
-        -- null_ls.builtins.diagnostics.rome,
     }
 }
 
