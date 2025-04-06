e81b075a2d3f693ed3f38b926dbc39dd07171949
Andreas Palm <andreaspalm98@gmail.com>
2025-04-06 17:49:34 +0200

    nvim: Fix nvim 0.11 deprecation warning about diagnostic signs definitions

--- .config/nvim/lua/zapling/diagnostic.lua
+++ .config/nvim/lua/zapling/diagnostic.lua
@@ -33,18 +33,21 @@ M.setup = function()
     vim.diagnostic.config({
         virtual_text = false,
         underline = true,
-        signs = true,
+        signs = {
+            text = {
+                [vim.diagnostic.severity.ERROR] = '',
+                [vim.diagnostic.severity.WARN] = '',
+                [vim.diagnostic.severity.INFO] = '',
+                [vim.diagnostic.severity.HINT] = '',
+            },
+        },
+
         float = {
             source = true,
             focusable = false,
         },
         severity_sort = true,
     }, nil)
-
-    vim.api.nvim_command('sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=')
-    vim.api.nvim_command('sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=')
-    vim.api.nvim_command('sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=')
-    vim.api.nvim_command('sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=')
 
     vim.api.nvim_command('autocmd CursorHold * lua vim.diagnostic.open_float(nil, {scope = \'line\'})')
     vim.api.nvim_command [[autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()]]
